c5340ad9de857f834a8dd8481c26ada70e89b7e7
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2020-09-30 15:59:21 +0200

    adding auth / compare login password

--- routes/user.js
+++ routes/user.js
@@ -35,13 +35,14 @@ userRouter.post('/login', async (req, res) => {
         const { error } = validateReq(req.body);
         if (error) return res.status(400).send(error.details[0].message);
 
-        const user = await User.findOne({
-            email: req.body.email,
-            password: req.body.password,
-        });
-        if (!user) return res.status(404).send('User not found');
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(404).send('Invalid email or password');
 
-        res.send(user);
+        const isValid = await bcrypt.compare(req.body.password, user.password);
+        if (!isValid)
+            return res.status(404).send('Invalid email or password..');
+
+        res.send(_.pick(user, ['name', 'email']));
     } catch (err) {
         return res.status(400).send(err.message);
     }
