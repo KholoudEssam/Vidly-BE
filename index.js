88ac679ec2551f1ca9a6cca0074b9f39e0ca852d
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2020-10-04 22:48:16 +0200

    unhandled rejection 2

--- index.js
+++ index.js
@@ -17,13 +17,22 @@ const { error, logger } = require('./middleware/error');
 
 const app = express();
 
+//ONLY SYNC CODE
 // process.on('uncaughtException', (ex) => {
 //     console.log('THERE IS UNHANDLED UNCAUGHT EXCEPTION.');
 //     logger.error(ex.message);
+//     process.exit(1);
 // });
 // throw new Error('There is an exception outside express context');
 
-// console.log(config.get('jwtPrivateKey'));
+//FOR ASYNC CODE - UNHANDLED PROMISE REJECTION
+// process.on('unhandledRejection', (ex) => {
+//     console.log('THERE IS UNHANDLED REJECTION');
+//     logger.error(ex.message);
+//     process.exit(1);
+// });
+// const p = Promise.reject(new Error('Something failed misrably'));
+// p.then(() => console.log('done'));
 
 if (!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR: jwtPrivateKey is not defined');
