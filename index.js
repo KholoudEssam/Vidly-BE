a1bc8b954a9b885b8dfad2b358377f73e50dedf5
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2020-10-07 20:44:19 +0200

    prepare the app to be deployed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 // require('./startup/logging')(app);
 
 const { logger } = require('./middleware/error');
